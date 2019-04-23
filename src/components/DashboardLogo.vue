42df839c29c145b3ad796a5e5597adf8960264bf
Rowan Littlefield <rowanlittlefield@ymail.com>
2019-04-22 20:38:08 -0400

    Create Dashboard and DashboardLogo components

---
+++ src/components/DashboardLogo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="dashboard-logo">
+    Crudepedia
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HelloWorld extends Vue {}
+</script>
+
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <DashboardLogo msg="Welcome to Your Vue.js + TypeScript App"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import DashboardLogo from '@/components/DashboardLogo.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    DashboardLogo,
+  },
+})
+export default class Home extends Vue {}
+</script>
