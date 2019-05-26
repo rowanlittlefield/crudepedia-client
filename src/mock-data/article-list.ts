4186b833244666e54252abc06fc17013172e18fb
Rowan Littlefield <rowanlittlefield@ymail.com>
2019-05-26 16:31:28 -0400

    Create article-list mock data

---
+++ src/mock-data/article-list.ts
@@ -0,0 +1,41 @@
+export default {
+  data: {
+    data: {
+      articles: [
+        {
+          id: '2',
+          title: 'Harald Hardrada',
+          pubDate: '2018-11-22T20:20:46.277451+00:00',
+          views: 12,
+          author: {
+            id: '1',
+            username: 'Todd',
+          },
+          imageUrl: '/assets/harald-hardrada.jpeg',
+        },
+        {
+          id: '3',
+          title: 'Harold Godwinson',
+          pubDate: '2019-05-08T00:43:46.332525+00:00',
+          views: 4,
+          author: {
+            id: '1',
+            username: 'Todd',
+          },
+          imageUrl: '/assets/harold-godwinson.jpeg',
+        },
+        {
+          id: '4',
+          title: 'William the Conqueror',
+          pubDate: '2019-05-08T00:46:32.781662+00:00',
+          views: 2,
+          author: {
+            id: '1',
+            username: 'Todd',
+          },
+          imageUrl: '/assets/william-the-conqueror.jpeg',
+        },
+      ],
+    },
+  },
+};
