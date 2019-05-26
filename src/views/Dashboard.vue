0791a97b84288dc7913a137fbc46e287cf021296
Rowan Littlefield <rowanlittlefield@ymail.com>
2019-05-26 16:44:04 -0400

    Add ArticleListItem list to Dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -11,22 +11,33 @@
         <a>{{ article.title }}</a>
         </router-link>
     </ul>
+    <ul>
+      <ArticleListItem 
+        v-for="article in mockArticleList"
+        :key="article.id"
+        :article="article"
+      />
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import DashboardLogo from '@/components/DashboardLogo.vue';
+import ArticleListItem from '@/components/ArticleListItem.vue';
 import graphQLService from '@/services/graphQL';
 import { getArticles } from '@/graphql/queries';
+import mockArticleList from '@/mock-data/article-list';
 
 @Component({
   components: {
     DashboardLogo,
+    ArticleListItem,
   },
 })
 export default class Dashboard extends Vue {
   private articleList = [];
+  private mockArticleList = mockArticleList.data.data.articles;
 
   public async mounted() {
     const query = getArticles();
