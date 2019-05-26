088f7ec10c333e20231e39d397fedd2c0996ddf9
Rowan Littlefield <rowanlittlefield@ymail.com>
2019-05-26 16:37:39 -0400

    Set default font family to Roboto

--- src/App.vue
+++ src/App.vue
@@ -5,4 +5,9 @@
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900&display=swap');
+
+body {
+  font-family: 'Roboto', Arial, sans-serif;
+}
 </style>
