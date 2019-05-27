a6e4fa6c9c41a79205090e02cf14d648e7de8c46
Rowan Littlefield <rowanlittlefield@ymail.com>
2019-05-27 12:45:07 -0400

    Remove commented out code

--- src/filters/datetime-elapsed.ts
+++ src/filters/datetime-elapsed.ts
@@ -3,12 +3,6 @@ import relativeTime from 'dayjs/plugin/relativeTime';
 
 dayjs.extend(relativeTime);
 
-// export default (time: string): string => {
-//   const isValidTime = time && dayjs(time).isValid();
-
-//   return isValidTime ? dayjs(time).fromNow() : '';
-// };
-
 export default (time: string): string => {
   const isValidTime = time && dayjs(time).isValid();
 
