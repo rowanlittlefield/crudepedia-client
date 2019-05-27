f0ca3f822d2fced73a104b7074402ce74b073754
Rowan Littlefield <rowanlittlefield@ymail.com>
2019-05-27 12:39:23 -0400

    Implement automagic imports for filters plugin

--- src/filters/index.ts
+++ src/filters/index.ts
@@ -1,7 +1,26 @@
-import datetimeElapsed from '@/filters/datetime-elapsed';
+import camelCase from 'lodash.camelcase';
+
+const files = require.context('./', true, /\.ts$/).keys();
+const filters: { name: string, filter: Function }[] = [];
+
+files.forEach((path) => {
+  if (path !== './index.ts') {
+    const fileName = path.replace(/\.\/|\.ts/g, '');
+    const filterModule = require(`@/filters/${fileName}`);
+
+    if (filterModule && !filterModule.default) {
+      throw new Error(`${fileName} must have a default export`);
+    }
+    
+    filters.push({
+      name: camelCase(fileName),
+      filter: filterModule.default,
+    });
+  }
+});
 
 export default {
   install(Vue: any) {
-    Vue.filter('datetimeElapsed', datetimeElapsed);
+    filters.forEach(({name, filter}) => Vue.filter(name, filter));
   },
 };
