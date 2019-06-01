4a58741380d5164015e9963ae827b1ecb768287e
Rowan Littlefield <rowanlittlefield@ymail.com>
2019-06-01 17:00:51 -0400

    Update operations to use fragments

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -1,29 +1,21 @@
+import { articleListItem } from '@/graphql/fragments';
+
 export const getArticles = (): string => `
-    query getArticles {
-      articles {
-        id
-        title
-        pubDate
-        views
-        author {
-          id
-          username
-        }
-      }
-    }`;
+  query getArticles {
+    articles {
+      ...articleListItem
+    }
+  }
+  ${articleListItem()}
+`;
 
 export const getArticle = (articleId: string): string => `
     query getArticle {
       article(id: ${articleId}) {
-        id
-        title
-        pubDate
+        ...articleListItem
         introduction
         description
-        views
-        author {
-          id
-          username
-        }
       }
-    }`;
+    }
+    ${articleListItem()}
+  `;
