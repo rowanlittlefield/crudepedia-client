c5379380da397e4ea0db86be106e5fbd6bdeb8f5
Rowan Littlefield <rowanlittlefield@ymail.com>
2019-08-24 17:25:08 -0400

    Consolidate changes from demo

--- src/graphql/mutations.ts
+++ src/graphql/mutations.ts
@@ -2,7 +2,7 @@ import { articleListItem } from '@/graphql/fragments';
 
 export const viewArticle: Readonly<string> = `
   mutation viewArticle($id: Int!) {
-    viewArticle(id: $id, input: {}) {
+    viewArticle(id: $id) {
       ok
       article {
         ...articleListItem
@@ -11,4 +11,4 @@ export const viewArticle: Readonly<string> = `
       }
     }
   }
-  ${articleListItem()}`;
+  ${articleListItem}`;
