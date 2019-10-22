f0ce56cef8b473f7f643d713942b1d6f7ac9112d
Rowan Littlefield <rowanlittlefield@ymail.com>
2019-10-21 20:32:03 -0400

    Reformat spec in datetime-elapsed testing suite

--- src/filters/datetime-elapsed.spec.ts
+++ src/filters/datetime-elapsed.spec.ts
@@ -5,8 +5,7 @@ describe('datetimeElapsed', () => {
     [''],
     [undefined],
     [null],
-  ])('returns \'-\' when passed a falsy value',
-    (time) => {
+  ])('returns \'-\' when passed a falsy value', (time) => {
       const actual = datetimeElapsed(time);
       expect(actual).toBe('-');
     });
