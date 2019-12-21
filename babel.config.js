601ff9d428a1a54928ada4b74d6e226a2ed59ceb
Rowan Littlefield <rowanlittlefield@ymail.com>
2019-12-21 14:38:54 -0500

    Use babel-plugin-module-resolver to resolve tsconfig aliases

--- babel.config.js
+++ babel.config.js
@@ -3,8 +3,14 @@ const path = require('path');
 module.exports = {
   presets: [
     '@vue/app'
-      ],
+  ],
   plugins: [
+    ["module-resolver", {
+      "root": ["./src"],
+      "alias": {
+        "@": "./src",
+      }
+    }],
     [path.resolve('./my-babel-graphql-plugin/index.js'), { extensions: '.gql' }],
   ],
 }
