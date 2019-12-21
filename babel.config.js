ebe22bbf26fa6208275b7fc30fb30bacbe78c46c
Rowan Littlefield <rowanlittlefield@ymail.com>
2019-12-21 13:58:23 -0500

    Add local my-babel-graphql-plugin to babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,10 @@
+const path = require('path');
+
 module.exports = {
   presets: [
     '@vue/app'
-  ]
+      ],
+  plugins: [
+    [path.resolve('./my-babel-graphql-plugin/index.js'), { extensions: '.gql' }],
+  ],
 }
